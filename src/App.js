ebeca0ff319c4546989e607438f8ca15e8dab49b
Maxxxnech <51296105+Maxxxnech@users.noreply.github.com>
2022-01-07 20:34:55 +0300

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+import Aside from "./components/Aside";
+import Footer from "./components/Footer";
 
-import './App.css';
-import Main from './components/Main';
+
 import data from "./data/articles.json";
-import Footer from './components/Footer';
-import Header from './components/Header';
+import navItems from "./data/navItems.json";
+import asideItems from "./data/asideItems.json";
+import footerItems from "./data/footerItems.json";
 
 function App() {
   return (
     <div className="App">
-        <Header text="header text"/>
+      <Header text="Блог (l3_t3 упр. 2 - 3)" />
+      <div className="wrapper" >
+        <Nav navItems={navItems} />
         <Main articles={data} />
-        <Footer text="footer text"/>
+        <Aside asideItems={asideItems} />
+      </div>
+      <Footer footerItems={footerItems} />
     </div>
   );
 }
