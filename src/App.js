420a1486b3dff70f9cb35b8b45257b1777f05fcd
Maxxxnech <51296105+Maxxxnech@users.noreply.github.com>
2022-01-08 16:44:22 +0300

    App.js fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 
 //***components***
+import React from 'react';
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 import Footer from "./components/Footer";
