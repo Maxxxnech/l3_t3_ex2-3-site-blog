637e9dc795909920b0ea0db93a73c0a7cf87a6d6
Maxxxnech <51296105+Maxxxnech@users.noreply.github.com>
2022-01-08 16:19:27 +0300

    dynamic footer, sticky nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import "./App.css";
+
+//***components***
 import Header from "./components/Header";
-import Main from "./components/Main";
-import Nav from "./components/Nav";
-import Aside from "./components/Aside";
+import Wrapper from "./components/Wrapper";
 import Footer from "./components/Footer";
 
-
-import data from "./data/articles.json";
+//***Data***
+import articles from "./data/articles.json";
 import navItems from "./data/navItems.json";
 import asideItems from "./data/asideItems.json";
 import footerItems from "./data/footerItems.json";
+import copyright from "./data/copyright.json";
 
 function App() {
   return (
     <div className="App">
-      <Header text="Блог (l3_t3 упр. 2 - 3)" />
-      <div className="wrapper" >
-        <Nav navItems={navItems} />
-        <Main articles={data} />
-        <Aside asideItems={asideItems} />
-      </div>
-      <Footer footerItems={footerItems} />
+      <Header text="Блог (тема 3 упр. 2 - 3)" />
+      <Wrapper
+        navItems={navItems}
+        articles={articles}
+        asideItems={asideItems}
+      />
+      <Footer footerItems={footerItems} copyright={copyright} />
     </div>
   );
 }
